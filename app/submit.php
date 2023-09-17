3ac6c3bf742135a2624a5558de5205f27c48ed5a
Dnidof <nidofonsecadiego@gmail.com>
2023-09-17 12:53:45 +0200

    cookie al registrarse arreglado

--- app/submit.php
+++ app/submit.php
@@ -1,3 +1,62 @@
+<?php
+    // https://www.freecodecamp.org/news/creating-html-forms/
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $nombre = $_POST["nombre"];
+        $telef = $_POST["telefono"];
+        $dni = $_POST["dni"];
+        $email = $_POST["email"];
+        $nacimiento = $_POST["nacimiento"];
+        $usuario = $_POST["usuario"];
+        $passwd = $_POST["passwd"];
+
+        $hostname = "db";
+        $username = "admin";
+        $password = "test";
+        $db = "database";
+
+        $conn = mysqli_connect($hostname,$username,$password,$db); 
+        if ($conn->connect_error) {
+        die("Database connection failed: " . $conn->connect_error);
+        }
+
+
+
+        $query = mysqli_query($conn, "SELECT * FROM usuarios WHERE usuario = '" . $usuario ."'")
+            or die (mysqli_error($conn));
+
+        $existeUsuario = false;
+
+        while ($row = mysqli_fetch_array($query)) {
+            $existeUsuario = $existeUsuario || ($row['usuario'] == $usuario);
+        }
+
+        // https://www.php.net/manual/es/mysqli.prepare.php en los comentarios, el de urso
+        if(!$existeUsuario){
+
+            $consulta = "INSERT INTO usuarios VALUES(?, ?, ?, ?, ?, ?, ?)";
+            $tipos = "sisssss";
+            $parametros = array($nombre, (int) $telef, $dni, $email, $nacimiento, $usuario, $passwd);
+            if($stmt = mysqli_prepare($conn, $consulta)){
+                    $stmt->bind_param($tipos, ...$parametros);
+                    $stmt->execute();
+                    $stmt->close();
+                    $mensaje = "Usuario creado";
+            }
+
+            // https://www.w3schools.com/php/func_network_setcookie.asp
+            $cookie_name = "user";
+            $cookie_value = $usuario;
+            setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 1 dia de duración
+        }else{
+            $mensaje = "Ya existe un usuario con ese nombre de usuario";
+        }
+        
+    }else{
+        header("/"); // redirigimos a inicio si no es POST
+        exit();
+    }
+    ?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -21,64 +80,8 @@
         </div>
         <div class="page mensaje">
             <?php
-            // https://www.freecodecamp.org/news/creating-html-forms/
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-                $nombre = $_POST["nombre"];
-                $telef = $_POST["telefono"];
-                $dni = $_POST["dni"];
-                $email = $_POST["email"];
-                $nacimiento = $_POST["nacimiento"];
-                $usuario = $_POST["usuario"];
-                $passwd = $_POST["passwd"];
-
-                $hostname = "db";
-                $username = "admin";
-                $password = "test";
-                $db = "database";
-
-                $conn = mysqli_connect($hostname,$username,$password,$db); 
-                if ($conn->connect_error) {
-                die("Database connection failed: " . $conn->connect_error);
-                }
-
-
-
-                $query = mysqli_query($conn, "SELECT * FROM usuarios WHERE usuario = '" . $usuario ."'")
-                    or die (mysqli_error($conn));
-
-                $existeUsuario = false;
-
-                while ($row = mysqli_fetch_array($query)) {
-                    $existeUsuario = $existeUsuario || ($row['usuario'] == $usuario);
-                }
-
-                // https://www.php.net/manual/es/mysqli.prepare.php en los comentarios, el de urso
-                if(!$existeUsuario){
-
-                    $consulta = "INSERT INTO usuarios VALUES(?, ?, ?, ?, ?, ?, ?)";
-                    $tipos = "sisssss";
-                    $parametros = array($nombre, (int) $telef, $dni, $email, $nacimiento, $usuario, $passwd);
-                    if($stmt = mysqli_prepare($conn, $consulta)){
-                            $stmt->bind_param($tipos, ...$parametros);
-                            $stmt->execute();
-                            $stmt->close();
-                            echo "Usuario creado";
-                    }
-
-                    // https://www.w3schools.com/php/func_network_setcookie.asp
-                    $cookie_name = "user";
-                    $cookie_value = $usuario;
-                    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 1 dia de duración
-                }else{
-                    echo "Ya existe un usuario con ese nombre de usuario";
-                }
-                
-            }else{
-                header("/"); // redirigimos a inicio si no es POST
-                exit();
-            }
-            ?>
+                echo $mensaje; 
+            ?> 
         </div>
     </body>
 </html>
