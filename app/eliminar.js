d6c270823ac0f978a45e70b339ffe80e8dbef85c
ImanolMM <imanolm.upv@gmail.com>
2023-09-18 08:43:01 +0200

    eliminar sin terminar

--- app/eliminar.js
+++ app/eliminar.js
@@ -1,4 +1,32 @@
-const boton = document.querySelector('.botonEliminar')
-boton.addEventListener('click', function() {
-    c;
+document.addEventListener('DOMContentLoaded', function () {
+  const botonEliminar = document.querySelectorAll('.botonEliminar');
+
+  botonEliminar.forEach(function (boton) {
+    boton.addEventListener('click', function () {
+      const id = boton.getAttribute('data-id');
+      
+      // Envía una solicitud POST al servidor para eliminar el registro
+      fetch('eliminar_registro.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ id: id }),
+      })
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          if (data.success) {
+            // Elimina el botón del DOM si se eliminó correctamente en la base de datos
+            boton.remove();
+          } else {
+            alert('Hubo un error al eliminar el registro.');
+          }
+        })
+        .catch(function (error) {
+          console.error('Error:', error);
+        });
+    });
   });
+});
