97d0aacb27ef69f0a7ffee27e65aedd5e1905f25
Dnidof <dnido001@ikasle.ehu.eus>
2023-11-24 18:48:54 +0100

    cambios header 2

--- app/crearEvento.php
+++ app/crearEvento.php
@@ -1,86 +1,82 @@
-<?php 
-        session_start();
-        include("functionsJWT.php"); 
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Eventify</title>
-        <link rel="stylesheet" href="crearEvento.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
+<?php
+    session_start();
+    include("functionsJWT.php"); 
 
-        <!-- Fuente de letra roboto de Google  https://fonts.google.com/specimen/Roboto -->
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    </head>
-    <body>
-      
-      <div class="page">
-        <?php
 
-        
-        if (!isset($_SESSION['token'])){
-            $_SESSION['token'] = bin2hex(random_bytes(32));
-        }  
-        $hostname = "db";
-        $username = "admin";
-        $password = "test";
-        $db = "database";
+    if (!isset($_SESSION['token'])){
+        $_SESSION['token'] = bin2hex(random_bytes(32));
+    }  
+    $hostname = "db";
+    $username = "admin";
+    $password = "test";
+    $db = "database";
 
-        $conn = mysqli_connect($hostname,$username,$password,$db);
-        if ($conn->connect_error) {
-        die("Database connection failed: " . $conn->connect_error);
-        }
+    $conn = mysqli_connect($hostname,$username,$password,$db);
+    if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+    }
 
 
 
-        $query = mysqli_query($conn, "SELECT * FROM usuarios")
-          or die (mysqli_error($conn));
-        include("navbar.php");
+    $query = mysqli_query($conn, "SELECT * FROM usuarios")
+        or die (mysqli_error($conn));
 
-        $usuarioAct = getUsuarioCookie();   // Lo obtengo para que se actualice su cookie
-                                            // y que no caduque
-        ?>
-        
-        <div class="formbox">
-            <div class="form-title">
-                Crear evento
+
+    $usuarioAct = getUsuarioCookie();   // Lo obtengo para que se actualice su cookie
+                                        // y que no caduque
+    include("navbar.php");
+    echo '
+        <head>
+            <title>Eventify</title>
+            <link rel="stylesheet" href="crearEvento.css">
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+
+            <!-- Fuente de letra roboto de Google  https://fonts.google.com/specimen/Roboto -->
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        </head>
+        <body>
+            
+            <div class="page">
+            <div class="formbox">
+                <div class="form-title">
+                    Crear evento
+                </div>
+                <p class="desc">
+                    Construya su evento aqui
+                </p>
+                <!-- Alinear inputs https://stackoverflow.com/questions/4309950/how-to-align-input-forms-in-html -->
+                <form class="form" action="/submitEventos.php" id="form-crear" method="POST">
+                    <div class="linea-form">
+                        <p>Titulo</p>
+                        <input type="text" name="titulo">
+                    </div>
+                    <div class="linea-form">
+                        <p>Enunciado</p>
+                        <input type="text" name="enunciado">
+                    </div>
+                    <div class="linea-form">
+                        <p>Opcion 1 </p>
+                        <input type="text" name="opcion1">
+                    </div>
+                    <div class="linea-form">
+                        <p>Resultado 1</p>
+                        <input type="text" name="resultado1">
+                    </div>
+                    <div class="linea-form">
+                        <p>Opcion 2</p>
+                        <input type="text" name="opcion2">
+                    </div>
+                    <div class="linea-form">
+                        <p>Resultado 2</p>
+                        <input type="text" name="resultado2">
+                    </div>
+                    <input type="hidden" name="token" value="'.$_SESSION['token'].'">
+                    <button type="submit" class="boton">Crear</button>
+                </form>
+
             </div>
-            <p class="desc">
-                Construya su evento aqui
-            </p>
-            <!-- Alinear inputs https://stackoverflow.com/questions/4309950/how-to-align-input-forms-in-html -->
-            <form class="form" action="/submitEventos.php" id="form-crear" method="POST">
-                <div class="linea-form">
-                    <p>Titulo</p>
-                    <input type="text" name="titulo">
-                </div>
-                <div class="linea-form">
-                    <p>Enunciado</p>
-                    <input type="text" name="enunciado">
-                </div>
-                <div class="linea-form">
-                    <p>Opcion 1 </p>
-                    <input type="text" name="opcion1">
-                </div>
-                <div class="linea-form">
-                    <p>Resultado 1</p>
-                    <input type="text" name="resultado1">
-                </div>
-                <div class="linea-form">
-                    <p>Opcion 2</p>
-                    <input type="text" name="opcion2">
-                </div>
-                <div class="linea-form">
-                    <p>Resultado 2</p>
-                    <input type="text" name="resultado2">
-                </div>
-                <input type="hidden" name="token" value="<?php echo $_SESSION['token'] ?? '' ?>">
-                <button type="submit" class="boton">Crear</button>
-            </form>
-
-        </div>
-      </div>
-    </body>
-</html>
+            </div>
+        ';
+?>
--- app/editarPost.php
+++ app/editarPost.php
@@ -1,77 +1,72 @@
-<?php 
+<?php
   session_start();
   if (!isset($_SESSION['token'])){
     $_SESSION['token'] = bin2hex(random_bytes(32));
   }
   include("functionsJWT.php"); 
+  
+  
+  
+
+  $hostname = "db";
+  $username = "admin";
+  $password = "test";
+  $db = "database";
+
+  $conn = mysqli_connect($hostname,$username,$password,$db);
+  if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+  }
+
+  
+  if(comprobarCookieUsuario()){
+    $usuario = getUsuarioCookie();
+  }else{
+    $usuario = "invitado";
+  }
+
   include("navbar.php");
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Eventify</title>
-        <link rel="stylesheet" href="editarPost.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-
-        <!-- Fuente de letra roboto de Google  https://fonts.google.com/specimen/Roboto -->
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    </head>
-    <body>
-      
-      <div class="page">
-      <div class="cabecera">
-          <img class="imagenSV" src="imagenes/logoSV.png"></img>
-          <h1 class="tituloInicio">Editar Eventos</h1>
-          <img class="imagenWIP" src="imagenes/logoWIP.png"></img>
+  $query = mysqli_query($conn, "SELECT * FROM eventos WHERE usuario='".$usuario."'")
+  or die (mysqli_error($conn));
+  echo '<!DOCTYPE html>
+  <html>
+      <head>
+          <title>Eventify</title>
+          <link rel="stylesheet" href="editarPost.css">
+          <link rel="preconnect" href="https://fonts.googleapis.com">
+  
+          <!-- Fuente de letra roboto de Google  https://fonts.google.com/specimen/Roboto -->
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+          <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      </head>
+        
+        <div class="page">
+        <div class="cabecera">
+            <img class="imagenSV" src="imagenes/logoSV.png"></img>
+            <h1 class="tituloInicio">Editar Eventos</h1>
+            <img class="imagenWIP" src="imagenes/logoWIP.png"></img>
+          </div>
+          <script src="eliminarEvento.js"></script>
+          ';
+  while ($row = mysqli_fetch_array($query)) {
+    echo "
+    <div class='evento'>
+        <div class='barraUsuario'>
+          <form action='/editar.php' method='POST'>
+            <input id='valtitulo' name='titulo' type='hidden' value='".htmlspecialchars($row['titulo'], ENT_QUOTES)."'>
+            <button class='botonEditar'> Editar </button>
+          </form>
+          <button class='botonEliminar'> Eliminar evento </button>
         </div>
-        <?php
-          
-          
-          
+        <h2 class='tituloEvento no-overflow'>".htmlspecialchars($row['titulo'], ENT_QUOTES) ."</h2>
+        <input id='valtoken' type='hidden' name='token' value='".$_SESSION['token']."'>
+        <p class='descripcionEvento no-overflow'>".htmlspecialchars($row['enunciado'], ENT_QUOTES) ."</p>
+    </div>
+    ";
+  }
+  
 
-          $hostname = "db";
-          $username = "admin";
-          $password = "test";
-          $db = "database";
+  
+?>
 
-          $conn = mysqli_connect($hostname,$username,$password,$db);
-          if ($conn->connect_error) {
-            die("Database connection failed: " . $conn->connect_error);
-          }
-
-          
-          if(comprobarCookieUsuario()){
-            $usuario = getUsuarioCookie();
-          }else{
-            $usuario = "invitado";
-          }
-
-          
-          $query = mysqli_query($conn, "SELECT * FROM eventos WHERE usuario='".$usuario."'")
-          or die (mysqli_error($conn));
-          while ($row = mysqli_fetch_array($query)) {
-            echo "
-            <div class='evento'>
-                <div class='barraUsuario'>
-                  <form action='/editar.php' method='POST'>
-                    <input id='valtitulo' name='titulo' type='hidden' value='".htmlspecialchars($row['titulo'], ENT_QUOTES)."'>
-                    <button class='botonEditar'> Editar </button>
-                  </form>
-                  <button class='botonEliminar'> Eliminar evento </button>
-                </div>
-                <h2 class='tituloEvento no-overflow'>".htmlspecialchars($row['titulo'], ENT_QUOTES) ."</h2>
-                <input id='valtoken' type='hidden' name='token' value='".$_SESSION['token']."'>
-                <p class='descripcionEvento no-overflow'>".htmlspecialchars($row['enunciado'], ENT_QUOTES) ."</p>
-            </div>
-            ";
-          }
-          
-
-          
-        ?>
-      </div>
-      <script src="eliminarEvento.js"></script>
-    </body>
-</html>
